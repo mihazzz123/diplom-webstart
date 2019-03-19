d29cff88575d6c3a163a96fce8362606ca560a41
mihazzz <m.dokuchaev73@gmail.com>
2019-03-19 23:39:41 +0300

    Подключил Git

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready( function() { 
+
+// Гамбургер
+
+$('#btn').click(function() {
+  $('#menu').toggleClass('active');
+});
+
+
+  // var $btn = $('.menu');
+    
+  //   var $menu = $('.navigation');
+    
+  //   $btn.click(function(){
+
+  //       if($menu.hasClass('active')){
+        
+  //           $menu.slideDown('normal').removeClass('active');
+  //       }
+  //       else{
+            
+  //           $menu.slideUp('normal').addClass('active');
+  //       }
+  //   });
+
+
+
+});
+
